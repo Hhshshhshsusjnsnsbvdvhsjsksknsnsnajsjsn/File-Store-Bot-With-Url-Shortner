ff324f499d29d24d78f4974d09049a1688130d2a
qtmovie <150934706+qtmovie@users.noreply.github.com>
2024-05-16 14:01:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "9619481"))
   API_HASH = os.environ.get("API_HASH", "10effb30531c66d27b90f1e07f6bd071")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6423120715:AAHxlrO2Bj1T7QaJfjdpJGV-uhlyFwUT1FQ")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6423120715:AAHM2f2YrA2BoyiZ62fKB0haqrgsC8tw3XI")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Fgggffd_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002036697162"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ziplinker.net")

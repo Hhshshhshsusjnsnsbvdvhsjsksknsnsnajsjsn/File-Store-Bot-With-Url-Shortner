e9c7d9a9985463d2124baed397b8c41c108a67de
Hhshshhshsusjnsnsbvdvhsjsksknsnsnajsjsn <koushalprasad731@gmail.com>
2024-08-31 17:53:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,15 +1,15 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "9619481"))
-  API_HASH = os.environ.get("API_HASH", "10effb30531c66d27b90f1e07f6bd071")
+  API_ID = int(os.environ.get("API_ID", "29534418"))
+  API_HASH = os.environ.get("API_HASH", "5f15dd792990ade40a43ae17413b422f")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6423120715:")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Fgggffd_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002036697162"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "api.shareus.io")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "X8KgVEykIqhQIXefXcmrJb9aSPt2")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6624919731"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://amanprasad066:5TQrQGZu6phZhXmF@cluster0.jalye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "2012457784"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://fffworld57:Or97XLuv6P6GvtFZ@cluster0.u34tcer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002135831842"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())

c7d3d8b3824cdcc58acdcccd4a80328a4782decc
Hhshshhshsusjnsnsbvdvhsjsksknsnsnajsjsn <koushalprasad731@gmail.com>
2024-12-13 01:33:58 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,42 +1,38 @@
 import asyncio
-import requests
-import string
-import random
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-from handlers.helpers import str_to_b64
-
-async def reply_forward(message: Message, file_id: int):
-    try:
-        await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
-            disable_web_page_preview=True,
-            quote=True
-        )
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        await reply_forward(message, file_id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
+    """
+    Forwards or copies a media message to a user.
+    """
     try:
-        if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
-        elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id)
+        if Config.FORWARD_AS_COPY:
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_id=file_id)
+        else:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
-        await message.delete()
+        return await media_forward(bot, user_id, file_id)
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def send_media(bot: Client, user_id: int, file_id: int):
+    """
+    Forwards media and schedules it for deletion after 30 minutes.
+    """
+    # Forward the media
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
+
+    # Schedule the message for deletion after 30 minutes
     asyncio.create_task(delete_after_delay(sent_message, 1800))
 
-async def delete_after_delay(message, delay):
+async def delete_after_delay(message: Message, delay: int):
+    """
+    Deletes a message after a specified delay (in seconds).
+    """
     await asyncio.sleep(delay)
-    await message.delete()
+    try:
+        await message.delete()
+    except Exception as e:
+        print(f"Failed to delete message: {e}")

74e6444befe953d9fac5c7db04cc49d13bf5fb00
Hhshshhshsusjnsnsbvdvhsjsksknsnsnajsjsn <koushalprasad731@gmail.com>
2024-12-17 00:16:39 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -32,7 +32,6 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     # Forward all media messages first
     sent_messages = []
-    for file_id in file_ids:
         sent_message = await media_forward(bot, user_id, file_id)
         sent_messages.append(sent_message)
 
